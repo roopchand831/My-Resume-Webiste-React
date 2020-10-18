51610e11cb1bf3d751d82c435c7133b5771150e7
Roopchand <roopchand831@gmail.com>
2020-10-18 22:08:58 +0530

    Updates

---
+++ precache-manifest.839efc781d3a99970134722e44bc8359.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ac5ec3c08add19301ba195b7615312",
+    "url": "/My-Resume-Webiste-React/index.html"
+  },
+  {
+    "revision": "a1afadc9ada55aa06e5d",
+    "url": "/My-Resume-Webiste-React/static/css/main.9af5024b.chunk.css"
+  },
+  {
+    "revision": "78fd3583f18410eddae4",
+    "url": "/My-Resume-Webiste-React/static/js/2.d91cdc32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-Resume-Webiste-React/static/js/2.d91cdc32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1afadc9ada55aa06e5d",
+    "url": "/My-Resume-Webiste-React/static/js/main.a0158fa0.chunk.js"
+  },
+  {
+    "revision": "39147e306757f4b4ca2f",
+    "url": "/My-Resume-Webiste-React/static/js/runtime-main.a44d1f87.js"
+  },
+  {
+    "revision": "2bb633c50a4c4d8cdca67b73df2544ec",
+    "url": "/My-Resume-Webiste-React/static/media/contact.2bb633c5.jpg"
+  },
+  {
+    "revision": "9d98681e21cfd5664a7422000186476b",
+    "url": "/My-Resume-Webiste-React/static/media/github-logo.9d98681e.png"
+  },
+  {
+    "revision": "dcf0373a0805c47b3b8e84211f5d3699",
+    "url": "/My-Resume-Webiste-React/static/media/homePage.dcf0373a.jpg"
+  },
+  {
+    "revision": "204a829a0091e4b2fc4e3af316ebb3ec",
+    "url": "/My-Resume-Webiste-React/static/media/website-resume-1.204a829a.jpg"
+  }
+]);
